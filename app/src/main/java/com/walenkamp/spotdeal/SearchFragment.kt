1c944b2b62e91f0bf2dfe436f69e777692656792
simonwalenkamp <sinnak.sinnak@hotmail.com>
2018-10-23 20:27:22 +0200

    progress

--- app/src/main/java/com/walenkamp/spotdeal/SearchFragment.kt
+++ app/src/main/java/com/walenkamp/spotdeal/SearchFragment.kt
@@ -1,12 +1,20 @@
 package com.walenkamp.spotdeal
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
 
 class SearchFragment : Fragment() {
+
+    // Drawer menu instance
+     private lateinit var mDrawerLayout: DrawerLayout
+
 
     companion object {
         fun newInstance(): SearchFragment {
@@ -14,12 +22,51 @@ class SearchFragment : Fragment() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.search_fragment, container, false)
+        val view = inflater.inflate(R.layout.search_fragment, container, false)
+        mDrawerLayout = view.findViewById(R.id.drawer_layout)
+
+        (activity as AppCompatActivity).setSupportActionBar(view.findViewById(R.id.toolbar))
+        val actionbar: ActionBar? = (activity as AppCompatActivity).supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+
+        mDrawerLayout.addDrawerListener(
+            object : DrawerLayout.DrawerListener {
+                override fun onDrawerStateChanged(newState: Int) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onDrawerClosed(drawerView: View) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onDrawerOpened(drawerView: View) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+            }
+        )
+        return view
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.drawer_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
